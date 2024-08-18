a3871f4749e200960a63b23eac7005ad5ea7cb3b
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-08-18 16:11:47 -0500

    Trying google auth

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    signInWithPopup,
+    signOut,
+    GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, getDoc, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,3 +20,29 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+
+export const googleLogin = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        await setDoc(
+            doc(db, "users", user.uid),
+            { uid: user.uid },
+            { merge: true }
+        );
+    } catch (error) {
+        alert(error.message);
+    }
+};
+
+export const logout = async () => {
+    try {
+        await signOut(auth);
+    } catch (error) {
+        alert(error.message);
+    }
+};
